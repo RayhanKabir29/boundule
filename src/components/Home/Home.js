0966112977c5f701b8400ab52001fd4f5a7bc096
RayhanKabir29 <rayhan.kabir29@gmail.com>
2021-10-29 18:36:29 +0600

    firebase

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,7 +4,7 @@ import './Home.css';
 const Home = () => {
     return (
         <div>
-            
+            <h2>This Is Home</h2>
         </div>
     );
 };
---
+++ src/components/Login/Firebase/firebase.init.js
@@ -0,0 +1,6 @@
+import firebaseConfig from "./firebase.config"
+
+const initializeAuthenticatio = () =>{
+    initializeApp(firebaseConfig)
+}
+export default initializeAuthenticatio;
