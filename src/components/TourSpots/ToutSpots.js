b296d06628040e32a0cd5731a0788b48c2af1ff7
RayhanKabir29 <rayhan.kabir29@gmail.com>
2021-10-29 16:16:25 +0600

    add components

---
+++ src/components/TourSpots/ToutSpots.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './TourSpots.css';
+
+const ToutSpots = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ToutSpots;
